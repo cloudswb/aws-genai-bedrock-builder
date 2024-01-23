1a111ddb4e00179ca98f0f16df9c9150cf019bf0
Peiwen Li <impw@amazon.com>
2024-01-23 16:58:58 +0800

    style: update agent chat

--- src/frontend/src/pages/chatagent/index.jsx
+++ src/frontend/src/pages/chatagent/index.jsx
@@ -1,293 +1,272 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import React, { createRef, useState } from 'react';
+import React, { createRef, useState } from 'react'
 // import ReactHtmlParser from 'react-html-parser';
 // import SyntaxHighlighter from 'react-syntax-highlighter';
 // import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { createRoot } from 'react-dom/client';
-import crypto from 'crypto-js'
 import {
+  Box,
   Button,
   Container,
   ContentLayout,
-  SpaceBetween,
-  Textarea,
-  ColumnLayout,
-  Box,
+  Grid,
+  Icon,
+  Input,
   Link,
-  FormField,
-  Select,
-  Header
-} from '@cloudscape-design/components';
+  SpaceBetween,
+  Spinner,
+} from '@cloudscape-design/components'
+import { createRoot } from 'react-dom/client'
+import '../../styles/base.scss'
+import { CustomAppLayout, Navigation } from '../commons/common-components'
+import Markdown from '../commons/markdown.tsx'
 import {
+  GetUserName,
   PageHeader,
   getSignedPostHeader,
-  GetUserName
-} from '../details/common-components.jsx';
-import {
-  CustomAppLayout,
-  Navigation
-} from '../commons/common-components';
-import Markdown from '../commons/markdown.tsx';
-import ToolsContent from '../details/tools-content.jsx';
-import '../../styles/base.scss';
+} from '../details/common-components.jsx'
+import ToolsContent from '../details/tools-content.jsx'
 // import { config } from 'process';
-import { config } from '../../../config';
+import { config } from '../../../config'
 
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require('uuid')
 
 // const [selectedValue, setSelectedValue] = useState('');
 function FeaturesSpotlightFooter() {
   return (
-    <Box textAlign="center">
-      <Link href="#" variant="primary">
+    <Box textAlign='center'>
+      <Link href='#' variant='primary'>
         View all posts
       </Link>
     </Box>
-  );
+  )
 }
 
-const sessionId = uuidv4();
+const sessionId = uuidv4()
 const ParentComponent = () => {
+  const [submitText, setSubmitText] = useState('Send')
+  const [submitState, setSubmitState] = useState(true)
 
-  const [submitText, setSubmitText] = useState('Submit');
-  const [submitState, setSubmitState] = useState(true);
-
-  const [conversionItems, setConversionItems] = useState([]);
-  const [ask, setAsk] = useState(null);
-
-  // const [searchMessage, setSearchMessage] = useState('');
-  const handleAddConversionItem = (props) => {
-
-
-    // postData();
-    send();
-  };
+  const [conversionItems, setConversionItems] = useState([])
+  const [ask, setAsk] = useState(null)
 
   // const llm_modes = [
   //   { value: 'anthropic.claude-v2', label: 'Claude2' },
   // ];
 
   const prompts = [
-    { value: 'Human: you are a power and honest assistant, you need answer questions in <QUESTION> tag and output the purely answer directly ${ask}.\n Assistant:', label: 'Default' },
+    {
+      value:
+        'Human: you are a power and honest assistant, you need answer questions in <QUESTION> tag and output the purely answer directly ${ask}.\n Assistant:',
+      label: 'Default',
+    },
   ]
 
-  const [selectedPromptValue, setSelectedPromptValue] = useState(prompts[0].value);
+  const [selectedPromptValue, setSelectedPromptValue] = useState(
+    prompts[0].value,
+  )
   // const [selectedLLMValue, setSelectedLLMValue] = useState('anthropic.claude-v2');
 
-  const agentId = config.BEDROCK_AGENT_ID;
-  const agentAliasId = config.BEDROCK_AGENT_ALIAS_ID;
+  const agentId = config.BEDROCK_AGENT_ID
+  const agentAliasId = config.BEDROCK_AGENT_ALIAS_ID
 
-  const endSession = false;
+  const endSession = false
 
   // https://2uzuvw7m5ogfo3zobmn44oc4km0kahwd.lambda-url.us-east-1.on.aws/
   async function send() {
-    const method = 'POST';
+    const method = 'POST'
 
-    setSubmitText('Waitting')
+    setSubmitText('Loading')
     setSubmitState(true)
-    
-    const host = config.LambdaAgentInvoke_URL;
-    const region = config.REGION;
-    const base = config.Lambda_URL_BASE;
 
+    const host = config.LambdaAgentInvoke_URL
+    const region = config.REGION
+    const base = config.Lambda_URL_BASE
 
-    const userName = GetUserName();
+    const userName = GetUserName()
 
     // const request_parameters = `{"prompt": "${ask.replace(/\n/g, "<br>")}", "agentId": "${agentId}", "agentAliasId": "${agentAliasId}", "sessionId": "${sessionId}", "endSession": "${endSession}", "username": "${userName}"}`;
     const request_parameters = JSON.stringify({
-      "prompt": ask, 
-      "agentId": agentId, 
-      "agentAliasId": agentAliasId, 
-      "sessionId": sessionId, 
-      "endSession": endSession, 
-      "username": userName
-    });
+      prompt: ask,
+      agentId: agentId,
+      agentAliasId: agentAliasId,
+      sessionId: sessionId,
+      endSession: endSession,
+      username: userName,
+    })
     let headers = {}
-    if(config.AUTH.toLowerCase() == 'iam'){
-      headers = await getSignedPostHeader(host, region, request_parameters);
+    if (config.AUTH.toLowerCase() == 'iam') {
+      headers = await getSignedPostHeader(host, region, request_parameters)
+    } else {
+      headers = { auth: 'cognito' }
     }
-    else{
-      headers = { 'auth': "cognito" }
-    }
 
-    console.log("headers:", headers)
-    
-    let streamedData = 'Loading'
+    console.log('headers:', headers)
 
+    let streamedData = 'Thinking...'
 
-    setConversionItems([{
-      user: "Question",
-      ask: ask,
-      llm: "Claude2",
-      answer: streamedData
-    }, ...conversionItems]);
+    setConversionItems([
+      {
+        user: 'Question',
+        ask: ask,
+        llm: 'Claude2',
+        answer: streamedData,
+      },
+      ...conversionItems,
+    ])
 
     const handleAnswerChange = (index, newAnswer) => {
-      console.log("newAnswer:", newAnswer)
+      console.log('newAnswer:', newAnswer)
       // Create a new array with the updated answer value
-      const updatedItems = [...conversionItems];
+      const updatedItems = [...conversionItems]
       updatedItems[index] = {
-        user: "Question",
+        user: 'Question',
         ask: ask,
-        llm: "Claude2",
-        answer: newAnswer
-      };
-  
+        llm: 'Claude2',
+        answer: newAnswer,
+      }
+
       // Update the state to trigger a re-render
-      setConversionItems([updatedItems[index], ...conversionItems]);
-    };
+      setConversionItems([updatedItems[index], ...conversionItems])
+    }
 
     // ************* SEND THE REQUEST *************
-    fetch(
-      base + host,
-      {
-        method: method,
-        headers: headers,
-        body: request_parameters, // JSON.stringify({ prompt: 'what is ec2?' }),
-      }
-    )
+    fetch(base + host, {
+      method: method,
+      headers: headers,
+      body: request_parameters, // JSON.stringify({ prompt: 'what is ec2?' }),
+    })
       .then((response) => {
-        streamedData = '';
-        console.log(response);
-        console.log(response.body);
+        streamedData = ''
+        console.log(response)
+        console.log(response.body)
 
-        const reader = response.body.getReader();
+        const reader = response.body.getReader()
         // Read data from the stream
         const readData = () => {
           return reader.read().then(({ done, value }) => {
             // console.log("value:", value)
             // console.log("done:", done)
             if (done) {
-              console.log('streamedData:', streamedData);
+              console.log('streamedData:', streamedData)
               // translateOption.translated = streamedData;
-              console.log("Stream complete");
+              console.log('Stream complete')
               // Update state with the response data
-              
-              setAsk('');
+
+              setAsk('')
               setSubmitState(false)
-              setSubmitText('Submit')
+              setSubmitText('Send')
             } else {
-              streamedData = streamedData + `${new TextDecoder().decode(value)}`;
-              
-              console.log("streamedData:", streamedData);
+              streamedData = streamedData + `${new TextDecoder().decode(value)}`
+
+              console.log('streamedData:', streamedData)
               handleAnswerChange(0, streamedData)
               // Continue reading data
-              return readData();
+              return readData()
             }
-          });
-        };
+          })
+        }
 
         // Start reading data from the stream
-        return readData();
-
-
+        return readData()
       })
       .catch((error) => {
-        console.error("Error:", error);
-        setSubmitText('Submit')
+        console.error('Error:', error)
+        setSubmitText('Send')
         setSubmitState(false)
-      });
-
+      })
   }
 
+  const loading = submitText === 'Loading'
 
   return (
     <ContentLayout>
-      <SpaceBetween size="l">
-
+      <SpaceBetween size='l'>
         <Container>
-          <ColumnLayout columns={4} rows={2} variant="text-grid" >
-
-            <div>
-              <FormField label="Prompt Template" direction="vertical">
-                <Select
-                  autoFocus={true}
-                  expandToViewport={true}
-                  ariaLabel="Select Language"
-                  options={prompts}
-                  onChange={event => {
-                    setSelectedPromptValue(event.detail.selectedOption.value);
-                  }}
-                  selectedOption={prompts.find(option => option.value == selectedPromptValue)}
-                />
-              </FormField>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <Icon name='user-profile' />
+            <div style={{ flex: 1 }}>
+              <Input
+                placeholder='Please ask your question here'
+                value={ask}
+                disabled={loading}
+                onChange={({ detail }) => {
+                  // setAsk(detail.value.replace(/\n/g, "<br>"))
+                  setAsk(detail.value)
+                  setSubmitState(detail.value.trim() == '' ? true : false)
+                }}
+              />
             </div>
-            {/* <div>
-              <FormField label="LLM Model" direction="direction">
-                <Select
-                  autoFocus={true}
-                  expandToViewport={true}
-                  ariaLabel="Select LLM Model"
-                  options={llm_modes}
-                  onChange={event => {
-                    setSelectedLLMValue(event.detail.selectedOption.value);
-                  }}
-                  selectedOption={llm_modes.find(option => option.value == selectedLLMValue)}
-                />
-              </FormField>
-            </div> */}
-
-          </ColumnLayout>
-          <ColumnLayout>
-
-            <Textarea
-              placeholder="Message LLM"
-              rows={2}
-              value={ask}
-              onChange={({ detail }) => {
-                // setAsk(detail.value.replace(/\n/g, "<br>"))
-                setAsk(detail.value)
-                setSubmitState(detail.value.trim() == '' ? true : false);
-              }
-              }
-            />
-
-            <form onSubmit={event => event.preventDefault()}>
-              <SpaceBetween direction="horizontal" size="xs">
-                <Button variant="link" onClick={()=> {
-                  setAsk('');
-                  setSubmitState(true);
-                   }}>Clean</Button>
-                <Button variant="primary" onClick={() => handleAddConversionItem()} disabled={submitState}>{submitText}</Button>
-              </SpaceBetween>
-            </form>
-          </ColumnLayout>
-
-
-          {/* <Message /> */}
+            <Button
+              variant='primary'
+              onClick={send}
+              disabled={submitState}
+              loading={loading}
+            >
+              {submitText}
+            </Button>
+          </div>
+          <div
+            style={{
+              marginTop: '24px',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '20px',
+            }}
+          >
+            {conversionItems.length === 0 ? (
+              <div>Conversations will be displayed here.</div>
+            ) : (
+              conversionItems.map((item, index) => (
+                <div key={index}>
+                  {index !== 0 ? (
+                    <div
+                      style={{
+                        borderTop: '0.5px solid silver',
+                        marginBottom: '24px',
+                      }}
+                    />
+                  ) : null}
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '10px',
+                    }}
+                  >
+                    <Icon name='user-profile' />
+                    <div>{item.ask}</div>
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '10px',
+                    }}
+                  >
+                    <div style={{ alignSelf: 'start', margin: '14px 0' }}>
+                      <Icon name='video-on' />
+                    </div>
+                    {/* <div dangerouslySetInnerHTML={{ __html: item.answer }} /> */}
+                    <Markdown>{item.answer}</Markdown>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </Container>
 
-        {conversionItems.map((item, index) => (
-          <Container key={index}>
-            <Header variant="h4">{item.user}</Header>
-            <Box variant="p">{item.ask}</Box>
-            <Header variant="h4">{item.llm}</Header>
-            <Box variant="p">
-            <div>
-              {/* <div dangerouslySetInnerHTML={{ __html: item.answer }} /> */}
-              <Markdown>{item.answer}</Markdown>
-            </div>
-            </Box>
-          </Container>
-        ))}
-
-        <FeaturesSpotlightFooter />
+        {/* <FeaturesSpotlightFooter /> */}
       </SpaceBetween>
     </ContentLayout>
   )
-
-
-};
-
-
+}
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { toolsIndex: 0, toolsOpen: false };
-    this.appLayout = createRef();
+    super(props)
+    this.state = { toolsIndex: 0, toolsOpen: false }
+    this.appLayout = createRef()
   }
 
   render() {
@@ -295,23 +274,22 @@ class App extends React.Component {
       <CustomAppLayout
         ref={this.appLayout}
         content={
-          <ContentLayout header={
-            <PageHeader
-              buttons={[]}
-              header={"Agent Chat"}
-            />
-          }>
-            <ParentComponent/>
+          <ContentLayout
+            header={<PageHeader buttons={[]} header={'Agent Chat'} />}
+          >
+            <ParentComponent />
           </ContentLayout>
         }
         // breadcrumbs={<Breadcrumbs />}
-        navigation={<Navigation activeHref="#/distributions" />}
+        navigation={<Navigation activeHref='#/distributions' />}
         tools={ToolsContent[this.state.toolsIndex]}
         toolsOpen={this.state.toolsOpen}
-        onToolsChange={({ detail }) => this.setState({ toolsOpen: detail.open })}
+        onToolsChange={({ detail }) =>
+          this.setState({ toolsOpen: detail.open })
+        }
       />
-    );
+    )
   }
 }
 
-createRoot(document.getElementById('app')).render(<App />);
+createRoot(document.getElementById('app')).render(<App />)
